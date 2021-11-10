5ec68bfb60d7e4b34f30ed0cc5a27977500adf61
asmatar <deruelle.arthur@gmail.com>
2021-11-10 10:54:54 +0100

    header donne

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import { SearchIcon } from '@heroicons/react/solid';
+import { GlobeAltIcon, MenuIcon, SearchIcon, UserCircleIcon, UsersIcon } from '@heroicons/react/solid';
 import Image from 'next/image';
 import React from 'react';
 
@@ -17,7 +17,15 @@ const Header = () => {
                 <input type="text" placeholder='start your search' className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400' />
                 <SearchIcon className=' hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
             </div>
-            {/* <div>right</div> */}
+            <div className='flex space-x-4 items-center justify-end text-gray-500'>
+                <p className='hidden md:inline cursor-pointer'>because a host</p>
+                    <GlobeAltIcon className='h-6 cursor-pointer' />
+                <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+                    <MenuIcon  className='h-6'/>
+                    <UserCircleIcon  className='h-6'/>
+                </div>
+                <UsersIcon  className='h-6'/>
+            </div>
         </header>
     )
 }
