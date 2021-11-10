d9e2e878ec43ad6deed677a22b487baab79faa49
asmatar <deruelle.arthur@gmail.com>
2021-11-10 13:55:44 +0100

    large card

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import LargeCard from '../components/LargeCard';
 import Mediumcard from '../components/Mediumcard';
 import Smallcard from '../components/Smallcard';
 
@@ -46,6 +47,12 @@ export default function Home({exploreData, cardsData}) {
                 }
               </div>
         </section>
+        <LargeCard 
+          img='https://links.papareact.com/4cj'
+          title='The Greatest Outdoors'
+          description='Wishlist curated by Ainbnb'
+          buttonText='Get inspired'
+        />
       </main>
     </div>
   )
