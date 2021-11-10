d1156bf5c9409631a7e23513c12528417c488c4e
asmatar <deruelle.arthur@gmail.com>
2021-11-10 11:18:00 +0100

    banner

--- components/Banner.js
+++ components/Banner.js
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import Image from 'next/image';
+import React from 'react';
 const Banner = () => {
     return (
-        <div>
-            i'm the banner
+        <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2l-h-[700px]'>
+           <Image src="https://links.papareact.com/0fm" 
+           layout='fill'
+           objectFit='cover'
+           />
+           <div className='absolute top-1/2 w-full text-center'>
+               <p className='text-sm sm:text-lg '>Not sure where to go ? Perfect.</p>
+               <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold m-3 hover:shadow-xl active:scale-90 transition duration-150'> Im flexible</button>
+           </div>
         </div>
     )
 }
