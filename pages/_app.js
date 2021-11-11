4562e2d632b257aa9e208d9b1e085d87d01f04f0
asmatar <deruelle.arthur@gmail.com>
2021-11-11 12:46:44 +0100

    progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+//import progress bar from npm librairy
+import ProgressBar from '@badrap/bar-of-progress'
+import Router from 'next/router'
 import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
+// initialize progress bar with parameters
+const progress = new ProgressBar({
+  size:4,
+  color: "#FE595E",
+  className:'z-50',
+  delay: 100,
+})
+// we add on event listener, when the route change, the progress bar have to start
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
